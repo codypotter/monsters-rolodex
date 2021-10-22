a0f70d31c72f5548665f6a7625ab4e40de7820d1
Cody Potter <enjoipotter@gmail.com>
2021-10-21 18:30:25 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
-
+  
   constructor() {
     super()
     this.state = {
@@ -21,14 +21,19 @@ class App extends Component {
       }))
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
     const { monsters, searchField } = this.state
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({searchField: e.target.value}, () => console.log(this.state))}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />         
       </div>
